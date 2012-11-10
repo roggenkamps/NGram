e13b1900a4fd59319e13d5fed9af134d41eb24e2
Steve Roggenkamp <roggenkamps@acm.org>
2012-11-09 23:31:29 -0500

    Minor cleanup

--- src/main/scala/NGram.scala
+++ src/main/scala/NGram.scala
@@ -1,12 +1,38 @@
-package parser
-
 import scala.util.parsing.combinator._
-import scala.collection.immutable.StringOps
 import scala.collection.mutable.HashMap
 import scala.collection.immutable.List
 import java.io.BufferedReader
 import java.io.FileReader
 import java.math
+
+
+object NGram  {
+  val parser = new SymbolParser
+
+  def main( args: Array[String] ) {
+    val wordsInKey = 2
+    val numWords   = 200
+    if ( args.length < wordsInKey + 1 ) {
+      println( "usage:  NGram corpus word1 word2..." )
+    } else {
+      val dictionary = new Dictionary()
+      val reader= new BufferedReader(new FileReader( args(0)))
+      val parse_result = parser.parse( parser.symbols, reader ) match {
+	case parser.Success( ListSymbol(symlist), _ ) => symlist.sliding(wordsInKey+1).toList
+	case parser.Error( msg, _ )                   => { println( "Error: "+msg ); return }
+      }
+
+      parse_result map { symlist => dictionary.addSymbol( symlist ) }
+      val wordList = for ( i <- 1 to wordsInKey ) yield WordSymbol(args(i))
+      val startphrase = (wordList.head.toString /: wordList.tail)(_+" "+_.toString)
+      print( startphrase )
+      dictionary. genSymbols( wordList.toList,
+			     numWords - wordsInKey,
+			     70 - startphrase.length )
+      println()
+    }
+  }
+}
 
 abstract class  Symbol
 case class NilSymbol()                         extends Symbol {
@@ -31,13 +57,13 @@ case class ParEndSymbol( )                     extends Symbol {
 object noSymbol extends NilSymbol
 
 class SymbolParser extends RegexParsers {
-  def lineend: Parser[NilSymbol]    = """\r?\n""".r ^^ { s => NilSymbol()}
-  def number:  Parser[Symbol]       = """\d+""".r  ^^ { n => NumberSymbol( n ) }
-  def other:   Parser[OtherSymbol]  = """.""".r ^^ { c => OtherSymbol( c ) }
-  def paraend: Parser[ParEndSymbol] = lineend~lineend ^^ { l1 => ParEndSymbol() }
-  def symbols: Parser[ListSymbol]   = rep(symbol) ^^ { l => ListSymbol(l ) }
+  def lineend: Parser[NilSymbol]    = """\r?\n""".r               ^^ { s => NilSymbol()}
+  def number:  Parser[Symbol]       = """\d+""".r                 ^^ { n => NumberSymbol( n ) }
+  def other:   Parser[OtherSymbol]  = """.""".r                   ^^ { c => OtherSymbol( c ) }
+  def paraend: Parser[ParEndSymbol] = lineend~lineend             ^^ { l1 => ParEndSymbol() }
+  def space:   Parser[NilSymbol]    = whiteSpace                  ^^ { s => NilSymbol()}
   def symbol:  Parser[Symbol]       = word | number | space | paraend | lineend | other
-  def space:   Parser[NilSymbol]    = whiteSpace ^^ { s => NilSymbol()}
+  def symbols: Parser[ListSymbol]   = rep(symbol)                 ^^ { l => ListSymbol(l) }
   def word:    Parser[WordSymbol]   = """[A-Za-z][A-Za-z']*""" .r ^^ { w => WordSymbol( w.toLowerCase() ) }
 }
 
@@ -51,11 +77,9 @@ class SymbolFrequencies {
     symbolFreq match {
       case Some(freq) => { total = total + 1
 			   symbols.update(symbol, freq+1)
-//			  println( "addSymbol1: ["+symbol+ "->"+ freq+1)
 			 }
       case None       => { total = total + 1
                            symbols.update(symbol, 1)
-//			  println( "addSymbol2: ["+symbol+ "->"+ 1)
 			 }
     }
   }
@@ -65,20 +89,16 @@ class SymbolFrequencies {
       if ( iter.hasNext ) {
 	iter.next() match {
 	  case (key, value) => if ( selectVal <= value ) {
-//	    println( "getSymbol: symbol = " + key )
 	    key
 	  } else {
 	    getSymbol( iter, selectVal - value )
 	  }
 	}
-      } else {
-	noSymbol
-      }
+      } else noSymbol
     }
 
     val randVal    = Math.round( total * Math.random ).toInt
     val symbolIterator = symbols.iterator
-//    println( "selectSymbol: total = "+total+"  randVal = "+randVal)
     getSymbol( symbolIterator, randVal )
   }
     
@@ -112,8 +132,8 @@ class Dictionary extends HashMap[List[Symbol],SymbolFrequencies] {
       val outstr = nextSymbol.toString
       if ( lineSize - outstr.length -1 < 0 ) {
 	println;
-	print( " "+outstr )
-	genSymbols( nextList, number -1, 70-outstr.length-1 )
+	print( outstr )
+	genSymbols( nextList, number -1, 70-outstr.length )
       } else {
 	print( " "+outstr )
 	genSymbols( nextList, number -1, lineSize-outstr.length-1)
@@ -131,30 +151,5 @@ class Dictionary extends HashMap[List[Symbol],SymbolFrequencies] {
       case None             => noSymbol
     }
   }
-
 }
 
-object SymbolParser extends SymbolParser {
-  val parser = new SymbolParser
-
-  def main( args: Array[String] ) {
-    val wordsInKey = 3;
-    if ( args.length < wordsInKey ) {
-      println( "usage:  parser.SymbolParser corpus word1 word2..." )
-    } else {
-      val reader= new BufferedReader(new FileReader( args(0)))
-
-      val parse_result = parse( symbols, reader ) match {
-	case Success( ListSymbol(symlist), _ ) => symlist.sliding(wordsInKey) .toList
-	case Error( msg, _ ) => { println( "Error: "+msg ); return }
-      }
-
-      val dictionary = new Dictionary()
-
-      parse_result map { symlist => dictionary.addSymbol( symlist ) }
-      val wordList = for ( i <- 1 to wordsInKey-1 ) yield WordSymbol(args(i))
-      dictionary. genSymbols( wordList.toList, 200, 70 - args(1).length - args(2).length - 2 )
-      println()
-    }
-  }
-}
